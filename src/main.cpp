333dca277d896485cc0997acf5358ab73c32f685
DaiMysha <mlp.lemon.birdy@gmail.com>
2015-09-07 15:19:30 +0200

    Added code to add points
    Points everywhere

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 
 #include <SFML/Graphics.hpp>
 #include <GL/gl.h>
+#include <list>
+
+#include <time.h>
 
 #define WIDTH   640
 #define HEIGHT  480
@@ -19,11 +22,20 @@ void drawCross(sf::RenderTarget& window, const sf::Vector2f& p, const sf::Color&
     window.draw(line, 4, sf::Lines);
 }
 
+void addRandoms(std::list<sf::Vector2f>& l, int count) {
+    float x,y;
+    for(int i=0;i<count;++i) {
+        x = rand() % WIDTH;
+        y = rand() % WIDTH;
+        l.push_back(sf::Vector2f(x,y));
+    }
+}
+
 //font taken from http://www.fontspace.com/melifonts/sweet-cheeks
 int main(int argc, char** argv)
 {
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "QuadTree tester");
-
+    srand(time(0));
     int fps = 0;
     int elapsedFrames = 0;
     sf::Clock clock, pclock;
@@ -35,12 +47,14 @@ int main(int argc, char** argv)
     sf::Text text;
     text.setFont(font);
     text.setCharacterSize(16);
-    text.setPosition(580,10);
+    text.setPosition(550,10);
     text.setString("0");
     text.setColor(sf::Color::White);
 
     sf::View view;
     view.setSize(sf::Vector2f(WIDTH,HEIGHT));
+
+    std::list<sf::Vector2f> points;
 
     while (window.isOpen()) {
         sf::Event event;
@@ -53,15 +67,39 @@ int main(int argc, char** argv)
                     {
                         window.close();
                     } break;
+                    case sf::Keyboard::F1:
+                    {
+                        addRandoms(points,1);
+                    } break;
+                    case sf::Keyboard::F2:
+                    {
+                        addRandoms(points,10);
+                    } break;
+                    case sf::Keyboard::F3:
+                    {
+                        addRandoms(points,100);
+                    } break;
+                    case sf::Keyboard::F4:
+                    {
+                        addRandoms(points,1000);
+                    } break;
                     default: break;
+                }
+            } else if(event.type == sf::Event::MouseButtonPressed) {
+                if(event.mouseButton.button == sf::Mouse::Right) {
+                    points.clear();
                 }
             }
         }
-        sf::Vector2i mouse = sf::Mouse::getPosition(window);
+        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
+            sf::Vector2i mouse = sf::Mouse::getPosition(window);
+            points.push_back(sf::Vector2f(mouse.x,mouse.y));
+        }
 
         window.clear();
 
-        drawCross(window,sf::Vector2f(mouse.x,mouse.y));
+        for(sf::Vector2f p : points)
+            drawCross(window,p);
 
         window.draw(text);
         window.display();
@@ -75,7 +113,7 @@ int main(int argc, char** argv)
             //for some reason my compiler doesn't find to_string so..
             std::ostringstream str;
             str << (fps*2) << "\n";
-            str << "n : " << 0;
+            str << "n : " << points.size();
             text.setString(str.str());
         }
     }
