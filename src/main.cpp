fc6d1bea4a67eee89cc3529b8a696fa74776137c
DaiMysha <mlp.lemon.birdy@gmail.com>
2015-09-08 15:57:51 +0200

    Added more options

--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,12 @@
 
 #include <time.h>
 
+#include <DMUtils\QuadTree.hpp>
+
 #define WIDTH   640
 #define HEIGHT  480
+
+int n=0;
 
 void drawCross(sf::RenderTarget& window, const sf::Vector2f& p, const sf::Color& c = sf::Color::Green, float size = 5.0f) {
 
@@ -22,12 +26,27 @@ void drawCross(sf::RenderTarget& window, const sf::Vector2f& p, const sf::Color&
     window.draw(line, 4, sf::Lines);
 }
 
-void addRandoms(std::list<sf::Vector2f>& l, int count) {
+void drawBox(sf::RenderTarget& window, sf::Vector2i a, sf::Vector2i b, sf::Color c) {
+
+    sf::Vertex line[] = {
+        sf::Vertex(sf::Vector2f(a.x,a.y), c),
+        sf::Vertex(sf::Vector2f(b.x,a.y), c),
+        sf::Vertex(sf::Vector2f(b.x,b.y), c),
+        sf::Vertex(sf::Vector2f(a.x,b.y), c),
+        line[0]
+    };
+
+    window.draw(line, 5, sf::LinesStrip);
+}
+
+void addRandoms(DMUtils::QuadTree<sf::Vector2f>& l, int count) {
     float x,y;
     for(int i=0;i<count;++i) {
         x = rand() % WIDTH;
-        y = rand() % WIDTH;
-        l.push_back(sf::Vector2f(x,y));
+        y = rand() % HEIGHT;
+
+        l.emplace(x,y,x,y);
+        ++n;
     }
 }
 
@@ -54,8 +73,12 @@ int main(int argc, char** argv)
     sf::View view;
     view.setSize(sf::Vector2f(WIDTH,HEIGHT));
 
-    std::list<sf::Vector2f> points;
-
+    sf::Vector2i click1;
+    sf::Vector2i click2;
+    bool pressed = false;
+    bool collision = false;
+    int col_size = 0;
+    DMUtils::QuadTree<sf::Vector2f> points(WIDTH,HEIGHT);
     while (window.isOpen()) {
         sf::Event event;
         while (window.pollEvent(event)) {
@@ -66,6 +89,15 @@ int main(int argc, char** argv)
                     case sf::Keyboard::Escape:
                     {
                         window.close();
+                    } break;
+                    case sf::Keyboard::BackSpace:
+                    {
+                        n = 0;
+                        points.clear();
+                    } break;
+                    case sf::Keyboard::Delete:
+                    {
+                        n -= points.remove(DMUtils::physics::AABB<float>(click1.x,click1.y,click2.x - click1.x,click2.y - click1.y));
                     } break;
                     case sf::Keyboard::F1:
                     {
@@ -83,23 +115,57 @@ int main(int argc, char** argv)
                     {
                         addRandoms(points,1000);
                     } break;
+                    case sf::Keyboard::F5:
+                    {
+                        addRandoms(points,100000);
+                    } break;
                     default: break;
                 }
             } else if(event.type == sf::Event::MouseButtonPressed) {
+                if(event.mouseButton.button == sf::Mouse::Middle) {
+                    collision = !collision;
+                }
+                if(event.mouseButton.button == sf::Mouse::Left) {
+                    sf::Vector2i mouse = sf::Mouse::getPosition(window);
+                    points.emplace(mouse.x,mouse.y,mouse.x,mouse.y);
+                    ++n;
+                }
                 if(event.mouseButton.button == sf::Mouse::Right) {
-                    points.clear();
+                    pressed = true;
+                    click1 = sf::Mouse::getPosition(window);
+                }
+            } else if(event.type == sf::Event::MouseMoved) {
+                if(pressed) click2 = sf::Mouse::getPosition(window);
+                if(click1.x > click2.x) {
+                    auto i = click2.x;
+                    click2.x = click1.x;
+                    click1.x = i;
+                }
+                if(click1.y > click2.y) {
+                    auto i = click2.y;
+                    click2.y = click1.y;
+                    click1.y = i;
+                }
+            } else if(event.type == sf::Event::MouseButtonReleased) {
+                if(event.mouseButton.button == sf::Mouse::Right) {
+                    pressed = false;
                 }
             }
-        }
-        if(sf::Mouse::isButtonPressed(sf::Mouse::Left)) {
-            sf::Vector2i mouse = sf::Mouse::getPosition(window);
-            points.push_back(sf::Vector2f(mouse.x,mouse.y));
         }
 
         window.clear();
 
-        for(sf::Vector2f p : points)
-            drawCross(window,p);
+        for(auto& p : points.data())
+            drawCross(window,*p);
+        if(collision) {
+            auto col = points.query(DMUtils::physics::AABB<float>(click1.x,click1.y,click2.x - click1.x,click2.y - click1.y));
+            for(auto& n : col) {
+                drawCross(window,*n.data.get(),sf::Color::Blue);
+            }
+            col_size = col.size();
+        }
+
+        drawBox(window,click1,click2,sf::Color::Red);
 
         window.draw(text);
         window.display();
@@ -114,6 +180,7 @@ int main(int argc, char** argv)
             std::ostringstream str;
             str << (fps*2) << "\n";
             str << "n : " << points.size();
+            if(collision) str << "\ncol : " << col_size;
             text.setString(str.str());
         }
     }
